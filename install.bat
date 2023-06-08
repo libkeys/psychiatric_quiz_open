4abe867d3df3101a888cfea3a40728fc38d30dc5
Case <libkeys@gmail.com>
2023-06-09 01:55:34 +0300

    1.5

--- install.bat
+++ install.bat
@@ -1,2 +1,5 @@
-start npm i nodemon
-start npm i http-server -g
+start npm i nodemon -g & exit
+start npm i express -g & npm i express & exit
+start npm i http-server -g & exit
+exit
+
--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 start nodemon app.js
-cd ./dist
-start http-server 8080
-start http://127.0.0.1:8080
+start cd ./dist & http-server 5173
+start http://127.0.0.1:5173
+
